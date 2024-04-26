d9d54a00d4cb34252f78592a93267183f4078b70
LHY927 <liuhaiyue0927@gmail.com>
2024-04-26 22:26:43 +0200

    update find and experienced

--- filters.rb
+++ filters.rb
@@ -1,12 +1,21 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
-def find(id)
+  def find(id)
     # Your code Here
+    candidates.each do |candidate|
+      return candidate if candidate[:id] == target_id
+    end
+    nil # Return nil if no candidate with the given id is found
   end
   
   def experienced?(candidate)
     # Your code Here
+    if candidate.years_of_experience >= 2
+      return true
+    else
+      return false
+    end
   end
   
   def qualified_candidates(candidates)
